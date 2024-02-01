26632d3e7921f0ba8f1c13f851d97a4e3689035e
PaulQ-dev <paul-dev@mail.ru>
2024-02-01 22:01:34 +0300

    Updated cmake buildfiles

--- build/CMakeFiles/volga-vm.dir/DependInfo.cmake
+++ build/CMakeFiles/volga-vm.dir/DependInfo.cmake
@@ -11,8 +11,12 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/paul/Coding/ProjectVolga-VM/code/machine/machine.cpp" "CMakeFiles/volga-vm.dir/code/machine/machine.cpp.o" "gcc" "CMakeFiles/volga-vm.dir/code/machine/machine.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
